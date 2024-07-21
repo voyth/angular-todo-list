eff68e5d3097f64fb929b81deb5f3c206e79c5b0
joao sanches <joao.p.sanches@gmail.com>
2024-07-21 20:01:29 +0100

    feat: add modal template and styles

---
+++ src/app/_components/to-do-modal/to-do-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './to-do-modal.component.html',
+  styleUrl: './to-do-modal.component.scss'
+})
+export class ToDoModalComponent {
+
+}
