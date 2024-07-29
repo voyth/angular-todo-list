382eea3cb9e3257ace0883bcb87d74b1bcb56296
joao sanches <joao.p.sanches@gmail.com>
2024-07-29 19:18:06 +0100

    feat: update modal to view todos details

--- src/app/_components/to-do-details-modal/to-do-details-modal.component.ts
+++ src/app/_components/to-do-details-modal/to-do-details-modal.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ToDo } from '../../models/todos';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-to-do-details-modal',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './to-do-details-modal.component.html',
   styleUrl: './to-do-details-modal.component.scss'
 })
